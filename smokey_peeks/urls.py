607f49e018631356248595fc758c54bbf1d50944
celestial02 <jesstenebro123@gmail.com>
2026-04-01 16:52:40 +0800

    changes logo

--- smokey_peeks/urls.py
+++ smokey_peeks/urls.py
@@ -3,8 +3,10 @@ URL configuration for Smokey Peeks project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url="/static/main/img/log.png", permanent=True)),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
